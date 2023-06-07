ee9e4d9d50e9420dbfeefd3fb4d6f7d8b454031e
elcuervo <elcuervo@elcuervo.net>
2023-06-07 09:58:53 -0300

    Add includes in test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
+  def comments(post)
+    c = post.comments
+    c = c.includes(:string_translations) unless I18n.default_locale == I18n.locale
+
+    c.pluck(:text)
+  end
+
   test 'gets translated version via association' do
     post = Post.create(title: 'something')
 
@@ -9,10 +16,14 @@ class PostTest < ActiveSupport::TestCase
     comment.update!(text_es: 'español')
     comment.update!(text_ja: 'kaigi')
 
-    assert_equal ['english'], post.comments.pluck(:text)
+    assert_equal ['english'], comments(post)
 
     I18n.with_locale(:es) do
-      assert_equal ['español'], post.comments.pluck(:text)
+      assert_equal ['español'], comments(post)
+    end
+
+    I18n.with_locale(:ja) do
+      assert_equal ['kaigi'], comments(post)
     end
   end
 end
